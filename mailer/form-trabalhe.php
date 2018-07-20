ba9ae29cb653962796ce0875e06136ffae53043c
casa <casa@DESKTOP-FJT5F0L>
2018-07-20 17:57:47 -0300

    Projeto West Carnes progração formularios

--- mailer/form-trabalhe.php
+++ mailer/form-trabalhe.php
@@ -5,7 +5,7 @@ $btnEnviar = filter_input(INPUT_POST, 'btnEnviar', FILTER_SANITIZE_STRING);
 if ($btnEnviar):
     $nome = utf8_decode(strip_tags(trim($_POST['txtnome'])));
     $email = utf8_decode(strip_tags(trim($_POST['txtemail'])));
-    $phone = utf8_decode(strip_tags(trim($_POST['txtphone'])));    
+    $phone = utf8_decode(strip_tags(trim($_POST['txtphone'])));
     $mensage = filter_input(INPUT_POST, 'txtmsg', FILTER_SANITIZE_STRING);
     $arquivo = $_FILES["arquivo"];
 
@@ -16,25 +16,39 @@ if ($btnEnviar):
         $mailer->SMTPDebug = 1;
         $mailer->SMTPAuth = true;
         $mailer->Port = 587; //Indica a porta de conexão para a saída de e-mails
+        $mailer->SMTPSecure = false; // Define se é utilizado SSL/TLS - Mantenha o valor "false"
+        $mailer->SMTPAutoTLS = false; // Define se, por padrão, será utilizado TLS - Mantenha o valor "false"
         $mailer->CharSet = utf8_decode($mensage);
         $mailer->Host = 'mail.westcarnes.com.br'; //smtp.dominio.com.br
         $mailer->Username = 'contato@westcarnes.com.br';
         $mailer->Password = 'a1b2c3d4';
 
+        // DADOS DO REMETENTE
+        $mailer->Sender = "$email"; // Conta de email existente e ativa em seu domínio
+        $mailer->From = "$email"; // Sua conta de email que será remetente da mensagem
+        $mailer->FromName = "$nome"; // Nome da conta de email    
         $mailer->SetFrom("$email", "$nome"); //Seu e-mail
-//        $mailer->addAddress('inovepublicidadedf@gmail.com'); // Name is optional ok
-        $mailer->AddAddress('administrativo@westcarnes.com.br', 'West Carnes');
-        $mailer->AddAddress('contato@westcarnes.com.br', 'Junio');        
-        $mailer->AddReplyTo(' administrativo@westcarnes.com.br', 'West Carnes'); //Seu e-mail      ok
+        $mailer->AddAddress('administrativo@westcarnes.com.br', 'West Carnes'); //Seu e-mail             ok
+        
         $mailer->AddAttachment($arquivo['tmp_name'], $arquivo['name']);
-        $mailer->Subject = "Formulario Trabalhe Conosco"; //Assunto do e-mail 
+        // Definição de HTML/codificação
+        $mailer->IsHTML(true); // Define que o e-mail será enviado como HTML
+        $mailer->Subject = "West Carnes Trabalhe Conosco"; //Assunto do e-mail 
         //Define o corpo do email
+        $mailer->Body .= " Nome: " . $_POST['txtnome'] . "<br>";
+        $mailer->Body .= " Email: " . $_POST['txtemail'] . "<br>";
+        $mailer->Body .= " Telefone: " . $_POST['txtphone'] . "<br>";
+        $mailer->Body .= " descricao: " . nl2br($_POST['txtmsg']) . "<br>";
+
         $mailer->MsgHTML("$mensage");
         $mailer->Send();
         echo "<script>alert('Sr(a) $nome seus dados foram enviados com sucesso! Aguarde o nosso contato!');</script>";
         echo "<script>window.location = 'http://www.westcarnes.com.br/novo/trabalhe-conosco'</script>";
+        // Limpa os destinatários e os anexos
+        $mailer->ClearAllRecipients();
+        // Exibe uma mensagem de resultado do envio (sucesso/erro)
     } catch (phpmailerException $e) {
-        echo $e->errorMessage(); //Mensagem de erro costumizada do PHPMailer
+        echo " Error! Tente novamente mais tarde, obrigado! " . $e->errorMessage(); //Mensagem de erro costumizada do PHPMailer
     }
 endif;
 ?>
